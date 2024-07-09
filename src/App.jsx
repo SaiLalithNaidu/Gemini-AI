c7f85acc190df7ff9146b94a42863f56543f5838
SaiLalithNaidu <konasailalith18@gmail.com>
2024-07-09 17:27:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React  from 'react'
+import sidebar from './Components/Sidebar/sidebar'
+
+const App = () => {
+  return(<>
+    <sidebar/>
+  </>)
+}
+
+export default App
---
+++ src/Components/Sidebar/sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './sidebar.css'
+
+const sidebar = () => {
+  return (
+    <div className=''></div>
+  )
+}
+
+export default sidebar
